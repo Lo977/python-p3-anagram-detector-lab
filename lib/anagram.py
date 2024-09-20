8fbb29e24f54d36ee7e027c59d4dc415d1e89ff4
Buddha Lo <lo_977@yahoo.com>
2024-09-20 04:51:19 -0700

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word_letters = word
+
+    def match(self,words):
+        return[letters for letters in words if sorted(letters) == sorted(self.word_letters)]
+        # match_lits = []
+
+        # for word in words:
+        #     if sorted([char for char in word]) == self.word_letters:
+        #         match_lits.append(word)
+        # return match_lits
